12ad155ba6b44d0f873fb8bd3aa49327b713be54
Gabriel Dibble <gdibble@gmail.com>
2014-08-02 01:16:26 -0700

    var declaration modernization

--- backbone-ajaxretry.js
+++ backbone-ajaxretry.js
@@ -1,3 +1,4 @@
+/*global define*/
 /*!
  * Backbone.js ajaxRetry
  * https://github.com/gdibble/backbone-ajaxretry
@@ -6,11 +7,11 @@
 (function (define) { define(function (require, exports, module) { //'use strict';
 
 
-var _$ajax;
-var Backbone = require('backbone');
-var _        = require('underscore');
-//Defaults that can be overridden via set
-var settings = {
+var _, Backbone, _$ajax, settings;
+
+_        = require('underscore');
+Backbone = require('backbone');
+settings = { //Defaults that can be overridden via set
   base:         2.718281828,
   y:            0.25,
   retryCount:   3,
@@ -30,14 +31,17 @@ function setOptions(options) {
   settings = options;
 }
 
-//returns delay in milliseconds
+//delay in milliseconds
 function exponentialDelay(x) {
   return (Math.pow(settings.base, x) - settings.y) * 1000;
 }
 
 //hit retry limit
 function exhausted() {
-  var args = Array.prototype.slice.call(arguments, 0);
+  var args;
+
+  args = Array.prototype.slice.call(arguments, 0);
+
   _.extend(args[0], this);
   // console.log('exhausted', this.url);
   if (this.hasOwnProperty('exhaust')) {
@@ -48,7 +52,10 @@ function exhausted() {
 
 //recurse the ajax request
 function ajaxRetry(jqXHR) {
-  var self = this;
+  var self;
+
+  self = this;
+
   if (this.hasOwnProperty('retries')) {
     this.recursed = this.recursed === undefined ? 0 : this.recursed + 1;
     if ((jqXHR && jqXHR.status < 500) || this.recursed >= this.retries) {
@@ -84,6 +91,7 @@ if (!settings.onlyBackbone) {
   _$ajax = $.ajax;
   $.ajax = function (options) {
     var args;
+
     if (typeof options === 'string') {
       arguments[1].url = options; //in this case, options is actually the url passed to $.get/$.post
       args = sliceArguments(arguments[1]);
@@ -92,13 +100,17 @@ if (!settings.onlyBackbone) {
       args = sliceArguments(arguments);
       extender(args, options);
     }
+
     return _$ajax.apply($, args);
   };
 } else {
   //retry only Backbone ajax requests422
   Backbone.ajax = function (options) {
-    var args = sliceArguments(arguments);
+    var args;
+
+    args = sliceArguments(arguments);
     extender(args, options);
+
     return Backbone.$.ajax.apply(Backbone.$, args);
   };
 }
